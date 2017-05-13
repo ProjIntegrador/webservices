21cfe40d589ede3ba322511b4a3a5c266e8d8a57
Renato <renatoea@gmail.com>
2017-05-13 17:16:03 -0300

    Reajustes e testes na classe UsuarioAuth

--- src/main/java/br/com/hippo/daos/jdbc/UsuarioAuthJdbc.java
+++ src/main/java/br/com/hippo/daos/jdbc/UsuarioAuthJdbc.java
@@ -1,10 +1,18 @@
 package br.com.hippo.daos.jdbc;
+/*
+public class UsuarioAuthJdbc implements  {
 
-public class UsuarioAuthJdbc {
 
+    public ProdutoDaoJdbc(String driver, String url, String user, String pass) {
+        this.driver = driver;
+        this.url = url;
+        this.user = user;
+        this.pass = pass;
+    }
 
     String sql = "SELECT * FROM dbo.Usuario WHERE loginUsuario="+loginUsuario+" AND " +
             "senhaUsuario='"+senhaUsuario+"'";
 
 
 }
+*/
--- src/main/java/br/com/hippo/ws/UsuarioAuthServlet.java
+++ src/main/java/br/com/hippo/ws/UsuarioAuthServlet.java
@@ -1,31 +1,24 @@
 package br.com.hippo.ws;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import br.com.hippo.entities.UsuarioAuth;
 
+@WebServlet("/usuario/auth")
 public class UsuarioAuthServlet extends HttpServlet{
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String loginUsuario = "";
-        String senhaUsuario = "";
+        String loginUsuario = request.getParameter("loginUsuario");
+        String senhaUsuario = request.getParameter("senhaUsuario");
 
-        if (loginUsuario != null) {
-            loginUsuario = response.;
-        }
-
-        if (senhaUsuario != null) {
-            senhaUsuario = request.;
-        }
-
-        if (username.equals("Manisha") && password.equals("password")) {
-            return "Hello " + username + " to world of Jax WS - Valid User!";
+        if (loginUsuario.equals("admin") && senhaUsuario.equals("pipoca")) {
+            System.out.println("XYZ " + loginUsuario);
         } else {
-            return " User No Valid!";
+            System.out.println("ERRO");
         }
     }
-
 }
