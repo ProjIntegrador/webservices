824edfbdb8ab994b594f7f1708fcef814f49ca5c
renatoea <renatoea@gmail.com>
2017-05-31 22:24:42 -0300

    altera empacotamento para aplicação web #mvn clean package

--- src/main/java/br/com/hippo/HippoStartApplication.java
+++ src/main/java/br/com/hippo/HippoStartApplication.java
@@ -2,10 +2,21 @@ package br.com.hippo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HippoStartApplication {
-	public static void main(String[] args) {
+public class HippoStartApplication
+		extends SpringBootServletInitializer
+				{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HippoStartApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(HippoStartApplication.class, args);
 	}
+
 }
